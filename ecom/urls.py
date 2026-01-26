d2ce05284368cfd1c17428a80208f26f9b0d156b
Mayumi <mapa.labausa.up@phinmaed.com>
2026-01-26 11:44:59 +0800

    here

--- ecom/urls.py
+++ ecom/urls.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 from django.urls import path
-from blog.views import login_view, index, logout_view, portfolio_view, portfolio_main_view
+from blog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', login_view, name='login'),
-    path('home/', index, name='home'),
-    path('portfolio/<str:person>/main/', portfolio_main_view, name='portfolio_main'),
-    path('portfolio/<str:person>/', portfolio_view, name='portfolio'),
-    path('logout/', logout_view, name='logout'),
+
+    path('', views.login_view, name='login'),
+    path('home/', views.index, name='home'),
+
+    path('portfolio/<str:person>/', views.portfolio_view, name='portfolio'),
+    path('portfolio/<str:person>/main/', views.portfolio_main_view, name='portfolio_main'),
+
+    path('logout/', views.logout_view, name='logout'),
 ]
